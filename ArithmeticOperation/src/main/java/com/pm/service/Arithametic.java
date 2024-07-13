fcd2cf31d4e921d6a1dc61da6a1d40de4dc3a9c1
HP <HP@LAPTOP-LJEB6FU1>
2024-07-13 17:42:37 +0530

    Perfrom arithmetic operation

--- ArithmeticOperation/src/main/java/com/pm/service/Arithametic.java
+++ ArithmeticOperation/src/main/java/com/pm/service/Arithametic.java
@@ -2,5 +2,8 @@ package com.pm.service;
 
 public class Arithametic 
 {
-
+	public int sum(int i ,int j)
+	{
+		return i + j;
+	}
 }
---
+++ ArithmeticOperation/src/test/java/com/pm/service/TestArithematicOperation.java
@@ -0,0 +1,37 @@
+package com.pm.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Test;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+
+public class TestArithematicOperation 
+{
+	public static Arithametic ao;
+	
+    @BeforeAll
+	public static void setup()
+	{
+		System.out.println("TestArithematicOperation.setup()");
+		ao = new Arithametic();
+	}
+    
+    
+    @Test
+    public void setSumWithPositiveNumber()
+    {
+    	int val1 = 10;
+    	int val2 = 20;
+    	int excepted = 30;
+    	int actual = ao.sum(val1, val2);
+        assertEquals(excepted, actual);
+    }
+    
+    @AfterAll
+	public static void down()
+	{
+		System.out.println("TestArithematicOperation.down()");
+		ao = null;
+	}
+}
